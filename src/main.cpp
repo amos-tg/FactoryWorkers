3f64c2a0c13d591078c409956834fb9e68182323
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2026-04-08 19:55:00 -0400

    added test messages and signatures to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include "factory.h"
 #include <iostream>
 
+using namespace std;
+
+const char 
+  *TEST_PASSED { "Passed" },  
+  *EMPLOYEE_TESTS { "Test (employee class): " },
+  *PRODUCTION_WORKER_TESTS { "Test (productionWorker class): " },
+  *SHIFT_SUPERVISOR_TESTS { "Test (shiftSupervisor class): " },
+  *TEAM_LEADER_TESTS { "Test (teamLeader class): " },
+  *CONSTRUCTOR_CHAINING_TESTS { "Test (Constructor Chaining): " };
+
+void employee_tests(void);
+void production_worker_tests(void);
+void shift_supervisor_tests(void);
+void team_leader_tests(void);
+void constructor_chaining_tests(void);
+
 int main(void) {
-  std::cout << "Hello, Amos!" << std::endl;
+  void employee_tests(void);
+  cout << '\n';
+  void production_worker_tests(void);
+  cout << '\n';
+  void shift_supervisor_tests(void);
+  cout << '\n';
+  void team_leader_tests(void);
+  cout << '\n';
+  void constructor_chaining_tests(void);
+  cout << endl;
 
   return 0;
 }
